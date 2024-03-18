cad67988ee1fe27723c7b5f62c2bf645ae0814c5
Samantha Callie <Hoomee90@gmail.com>
2024-03-18 14:15:10 -0700

    complete yes attack text

--- QueenAttack/UserInterfaceModels/DisplayScript.cs
+++ QueenAttack/UserInterfaceModels/DisplayScript.cs
@@ -65,7 +65,7 @@ namespace QueenAttack.UserInterfaceModels
 			TypeLine("Until they make it.");
 			TypeLine($"({x}, {y}) looks just the same as every square they have seen thus far, but it is not the nameless piece's place to question orders.");
 			TypeLine("They get to work. Securing the area, deploying accommodations.");
-			TypeLine("Preparing themself for whatever may come next.", true);
+			TypeLine("Preparing themselves for whatever may come next.", true);
 			Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
 		}
 		
@@ -125,8 +125,9 @@ namespace QueenAttack.UserInterfaceModels
 		
 		internal static void YesAttack(int x, int y)
 		{
+			ConsoleColor queenTalk = ConsoleColor.Yellow;
 			TypeLine("After a couple days, the nameless chess piece has settled into their temporary dwelling.");
-			TypeLine("It's... nice. No rigid structure, no midnight emergency drills, no cruel instructors.");
+			TypeLine("It's... nice. No rigid schedule, no midnight emergency drills, no strick instructors.");
 			TypeLine("Just an open horizon and the pride of successfully serving their nation.", true);
 			
 			TypeLine("In their wonderment at these positive emotions, the nameless chess piece begins slacking.");
@@ -135,13 +136,13 @@ namespace QueenAttack.UserInterfaceModels
 			TypeLine("But the play's third act is about to begin, and their roll has no room for improv.");
 			TypeLine("There is only one way this story ends.", true);
 			
-			TypeLine("It happens not in the dead of night, but rather broad daylight.");
+			TypeLine("It happens not in the dead of night, but rather the blinding day.");
 			TypeLine("The nameless chess piece has just finished eating their breakfast rations when they see it.");
 			TypeLine("A lone figure. No more than a black speck on the horizon.");
 			TypeLine("Long distance optic instruments are quickly unloaded.");
 			TypeLine("But by the time the nameless chess piece looks again, they are gone.", true);
 			
-			TypeLine("However, right before they can dismiss it as their imagination, the figure reappears closer.");
+			TypeLine("However, the moment before they would've dismissed it as their imagination, the figure reappears closer.");
 			TypeLine("Much, much closer.");
 			TypeLine("She is tall, a body more wiry than lithe by way of poised muscles. Draped in a raggedy cloak, her face is perfectly blank.");
 			TypeLine("There is a blade in her hand, blacker than midnight, and a crown on her head.");
@@ -149,10 +150,29 @@ namespace QueenAttack.UserInterfaceModels
 			TypeLine("It is tarnished and worn, but unmistakable. The crown of a Queen.", true);
 			
 			TypeLine("The nameless piece reaches for their weapon, but in the space of a breath the Queen moves.");
-			TypeLine("The fight is over before it began.");
-			TypeLine("Black flashes, red splatters, and the nameless piece finds themselves fighting a losing battle to breathe through perforated lungs.");
+			TypeLine("The fight is over before it begins.");
+			TypeLine("Black flashes, red cascades, and the nameless piece finds themselves fighting a losing battle to breathe through perforated lungs.");
 			TypeLine("Standing loosely over them, the Queen's impassive face acquires a bitter sneer.");
-			TypeLine("\"Again? What're they even getting out of this?\" Her voice was scratchy, upper class accent crushed into something grating.", true, ConsoleColor.Black);
+			TypeLine("\"Again? What're they even getting out of this?\" Her voice is scratchy, upper class accent crushed into something grating.", true, queenTalk);
+			
+			TypeLine("As she steps over the nameless piece's prone form to begin rummaging through their tent, the Queen continues.");
+			TypeLine("\"At this point, I'm torn between thinking they're not actually collecting any data or are just astronomically stupid.\"", false, queenTalk);
+			TypeLine("\"What do you think? Cruel or incompetent?\" She says, reappearing in the nameless piece's spotty field of vision, newly seized bag slug over her shoulder.", false, queenTalk);
+			TypeLine("The nameless piece, of course, cannot respond. That doesn't stop the Queen from pausing, as if waiting for their answer.", true);
+			
+			TypeLine("\"Who am I even kidding. A good soldier would never speak ill of those who give them orders.\" She mutters, face softening into some unreadable emotion.", false, queenTalk);
+			TypeLine("\"The truth of the matter is that the only thing I can do is kill, and the only thing you can do is die.\"", false, queenTalk);
+			TypeLine("\"And it's all their fault.\" She hisses, though the nameless piece is barely conscious enough to hear.", false, queenTalk);
+			TypeLine("\"But nothing lasts forever. I will escape this cycle, and I will have my revenge. That is my promise.\"", true, queenTalk);
+			
+			TypeLine("The queen's face twists into a mockery of a smile. She leans down and closes the nameless piece's eyes.", false, queenTalk);
+			TypeLine("\"I'm not sorry for ending the life you almost had. But I'm sorry you never had a choice in the matter.\"", true, queenTalk);
+			
+			TypeLine("\"I'm sorry you were just a pawn.\"", true, queenTalk);
+			
+			TypeLine("And then it is over. Perhaps the fading sound of footsteps echo, but there is no longer anybody around to hear them.");
+			TypeLine("The data is invaluable, is it not?");
+			TypeLine($"The Queen was able to successfully attack the location of ({x}, {y})", true);
 		}
 	}
 }
